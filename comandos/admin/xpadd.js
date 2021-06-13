df940aebe380a611f153335f37fb43f6b4cd2a21
DidaS <didasoficial@gmail.com>
2021-06-13 16:29:54 +0100

    Added xpadd command

--- comandos/admin/xpadd.js
+++ comandos/admin/xpadd.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js');
-const { getXP, getLvL } = require('../manager');
+const { getXp, getLvL } = require('../../manager');
 
 module.exports = {
     name: 'xpadd',
     description: 'Adiciona xp a um utilizador',
-    execute(message) {
+    execute(message, args, target) {
         // maybe
+        if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send("Você não tem permissão para usar este comando");
+        if (!args) return;
+        let amount = parseFloat(args.slice(1).join().replace(/,/g, ""));
+        if (!amount) return message.channel.send("Você não especificou quanto xp quer dar");
+        getXp(target.id) = getXP(target.id) + amount
+        
         message.channel.send('Desu~')
     }
 }
--- comandos/users/rank.js
+++ comandos/users/rank.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { nextLvL, getXp, getLvL } = require('../manager');
+const { nextLvL, getXp, getLvL } = require('../../manager');
 
 module.exports = {
     name: 'rank',
