d9d3df9326b5592739d3c0949fd900fb295520a0
DidaS <didasoficial@gmail.com>
2021-06-13 16:56:42 +0100

    Fixed xpadd

--- comandos/admin/xpadd.js
+++ comandos/admin/xpadd.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const { getXp, getLvL } = require('../../manager');
+const xp = require('../../xp.json');
 
 module.exports = {
     name: 'xpadd',
     description: 'Adiciona xp a um utilizador',
     execute(message, args, target) {
-        // maybe
         if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send("Você não tem permissão para usar este comando");
         if (!args) return;
         let amount = parseFloat(args.slice(1).join().replace(/,/g, ""));
-        if (!amount) return message.channel.send("Você não especificou quanto xp quer dar");
-        getXp(target.id) = getXP(target.id) + amount
-        
-        message.channel.send('Desu~')
+        if (!amount) return message.channel.send("Você não especificou o utilizador");
+        xp[target.id].xp = xp[target.id].xp + amount
+
+        fs.writeFile("./xp.json", JSON.stringify(xp, null, 4), (err) => {
+            if(err) console.log(err)
+        })
     }
 }
