794cfcd5b2d3e00a185fbdaf41ca70b7d746c14a
DidaS <didasoficial@gmail.com>
2021-06-13 19:22:27 +0100

    Update search.js

--- comandos/users/search.js
+++ comandos/users/search.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: 'search'
-    description: 'Procurar um doujin no nhentai'
-    execute(message) {
-         if (!message.channel.nsfw) return message.channel.send("Este canal não permite conteudo nsfw!");
-            let search = 
-         message.channel.execute '\n https://nhentai.net/g/${numeros}'
+    name: 'search',
+    description: 'Procurar um doujin no nhentai',
+    execute(message, args) {
+        if (!message.channel.nsfw) return message.channel.send("Este canal não permite conteudo nsfw!");
+        if (!args[0]) return message.channel.send('Você não especificou o que devo procurar');
+        message.channel.send(`${message.author}\nhttps://nhentai.net/g/${args[0]}`);
     }
 }
