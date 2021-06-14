b103c62ca5efb194313d740eee4878b9e82591d3
Didas <didasoficial@gmail.com>
2021-06-14 15:38:40 +0100

    Temporary fix

--- comandos/admin/emoji-id.js
+++ comandos/admin/emoji-id.js
@@ -4,6 +4,12 @@ module.exports = {
     execute(message, args) {
         message.delete({timeout: 30})
         if (!message.member.hasPermission('MANAGE_EMOJIS')) return message.channel.send('Você não tem permissão para usar este comando');
-        message.channel.send(`\\${args[0]}`);
+        if (!args[0]) return message.reply('Você não especificou o emoji');
+        const regex1 = args[0].replace(/^<(a?):\w+:\d+>$/, '$1');
+        const regex2 = args[0].replace(/^<a?:(\w+):\d+>$/, '$1');
+        const regex3 = args[0].replace(/^<a?:\w+:(\d+)>$/, '$1');
+
+        console.log(regex1 + regex2 + regex3)
+        message.channel.send(`\\<${regex1}:${regex2}:${regex3}>`);
     }
 }
