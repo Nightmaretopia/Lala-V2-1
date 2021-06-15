25623537f7ce270ca2ea0bec1df109969638ceeb
Didas <didasoficial@gmail.com>
2021-06-15 10:39:02 +0100

    Fixed the emoji commands so you can get emojis from other servers now
    Worst way possible to do it

--- comandos/admin/emoji-id.js
+++ comandos/admin/emoji-id.js
@@ -5,11 +5,11 @@ module.exports = {
         message.delete({timeout: 30})
         if (!message.member.hasPermission('MANAGE_EMOJIS')) return message.channel.send('Você não tem permissão para usar este comando');
         if (!args[0]) return message.reply('Você não especificou o emoji');
-        const regex1 = args[0].replace(/^<(a?):\w+:\d+>$/, '$1');
-        const regex2 = args[0].replace(/^<a?:(\w+):\d+>$/, '$1');
-        const regex3 = args[0].replace(/^<a?:\w+:(\d+)>$/, '$1');
+        const regexa = args[0].replace(/^<(a?):\w+:\d+>$/, '$1');
+        const regexw = args[0].replace(/^<a?:(\w+):\d+>$/, '$1');
+        const regexd = args[0].replace(/^<a?:\w+:(\d+)>$/, '$1');
 
-        console.log(regex1 + regex2 + regex3)
-        message.channel.send(`\\<${regex1}:${regex2}:${regex3}>`);
+        console.log(regexa + regexw + regexd)
+        message.channel.send(`\\<${regexa}:${regexw}:${regexd}>`);
     }
 }
--- comandos/users/emoji.js
+++ comandos/users/emoji.js
@@ -2,20 +2,24 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'emoji',
-    description: 'Obtem o .png do emoji',
-    execute(message, args) {
+    description: 'Obtem a imagem do emoji',
+    execute(message, args, client) {
         message.delete({timeout: 30});
-        if (!args[0]) return message.channel.send('?');
-
+        if (!args[0]) return message.channel.send('Você não não especificou que emoji quer')
+        const regexa = args[0].replace(/^<(a?):\w+:\d+>$/, '$1');
         const regex = args[0].replace(/^<a?:\w+:(\d+)>$/, '$1');
-        const emojim = message.guild.emojis.cache.find((emj) => emj.name === args[0] || emj.id === regex);
+        let eurl = `https://cdn.discordapp.com/emojis/${regex}.png`;
+        if (regexa) {
+            eurl = `https://cdn.discordapp.com/emojis/${regex}.gif`
+        }
         
-        if (!emojim) return message.channel.send(`${args[0]} não é um emoji válido`);
+        if (!regex) return message.reply(`${args[0]} não é um emoji válido`)
+
         let emojiembed = new Discord.MessageEmbed()
             .setColor("#FF00EF")
-            .setImage(emojim.url)
+            .setImage(eurl)
             .setFooter(message.author.username, message.author.displayAvatarURL())
             .setTimestamp();
-        message.channel.send(message.author, emojiembed);
+        message.channel.send(emojiembed)
     }
 }
