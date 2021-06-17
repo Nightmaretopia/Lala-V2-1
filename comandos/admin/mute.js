fe0dd8e8cb33926b4d02b4cff9842789f9d6e897
Didas <didasoficial@gmail.com>
2021-06-17 14:45:07 +0100

    Added unmute

--- comandos/admin/mute.js
+++ comandos/admin/mute.js
@@ -1,16 +1,14 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 module.exports = {
     name: 'mute',
     description: 'Muta o membro mencionado',
     async execute(message, target, reasonarg, client) {
         if (!message.member.hasPermission('MUTE_MEMBERS')) return message.channel.send('Você não tem permissão para usar este comando');
-        if (!target) {
-            return message.channel.send("Você não especificou quem quer mutar")
-                .then(message => {
-                    message.delete({timeout: 3000})
-                })
-        }
+        if (!target) return message.channel.send("Você não especificou quem quer mutar")
+            .then(message => {
+                message.delete({timeout: 3000})
+            });
         if (message.guild.member(target).roles.highest.position > message.guild.members.resolve(client.user).roles.highest.position) return message.channel.send('Você não pode mutar um moderador');
         const muterole = message.guild.roles.cache.find(r => r.name == "Lala Mute");
         const pos = message.guild.members.resolve(client.user).roles.highest.position;
@@ -26,18 +24,18 @@ module.exports = {
                         })
                     }))
             })
-        }
+        };
         if (message.guild.member(target).roles.cache.has(muterole.id)) return message.channel.send("Este membro já está mutado")
             .then(message => {
                 message.delete({timeout: 3000})
-            })
+            });
         let mutembed = new Discord.MessageEmbed()
             .setColor("#FF0000")
             .setTitle(`Mutado ${target.tag}`)
-            .setDescription(`Membro foi mutado.\nRazão: ${reasonarg}`)
+            .setDescription(`${target.tag} foi mutado.\nRazão: ${reasonarg}`)
             .setTimestamp();
 
-        message.guild.member(target).roles.add(muterole)
-        message.channel.send(mutembed)
+        message.guild.member(target).roles.add(muterole);
+        message.channel.send(mutembed);
     }
 }
---
+++ comandos/admin/unmute.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const { prefix } = require('../../config.json');
+
+module.exports = {
+    name: 'unmute',
+    description: 'Tira o mute do membro mencionado',
+    async execute(message, target, client) {
+        if (!message.member.hasPermission('MUTE_MEMBERS')) return message.channel.send('Você não tem permissão para usar este comando');
+        if (!target) return message.channel.send('Você não especificou que quer mutar')
+            .then(message => {
+                message.delete({timeout: 3000})
+            });
+        if (message.guild.member(target).roles.highest.position > message.guild.resolve(client.user).roles.highest.position) return message.channel.send('Você não pode usar este comando em um moderador');
+        const muterole = message.guild.roles.cache.find(r => r.name == "Lala Mute");
+        if (!muterole) return message.channel.send(`O servidor não tem o mute configurado.\NUse \`${prefix}mute\` para configurar o mute.`);
+        if (!message.guild.member(target).roles.cache.has(muterole.id)) return message.channel.send('Este membro não está mutado')
+            .then(message => {
+                message.delete({timeout: 3000})
+            });
+        let unmutembed = new Discord.MessageEmbed()
+            .setColor(0x0000FF)
+            .setTitle(`Desmutado ${target.tag}`)
+            .setDescription(`${target.name} foi desmutado`)
+            .setTimestamp();
+        message.guild.member(target).roles.remove(muterole);
+        message.channel.send(unmutembed);
+    } 
+}
