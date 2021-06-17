b9fcf081e72f1ae3b35865bd2954d1c96e18aafb
Didas <didasoficial@gmail.com>
2021-06-17 15:36:47 +0100

    Added message delete logs

---
+++ events/bulk.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'messageDeleteBulk',
+    execute(message) {
+        let bulkembed = new Discord.MessageEmbed()
+            .setColor(0xfc5603)
+            .setAuthor("Purge")
+            .setDescription(`Apagadas **${message.size}** mensagens`);
+            
+        bot.channels.cache.get('454111723872321536').send(bulkembed);
+    }
+}
---
+++ events/message delete.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: 'messageDelete',
+    execute(message) {
+        if (message.channel.type == "dm") return;
+        if (message.channel.id === '826862921405300786') return;
+        if (message.author.bot) return;
+        if (message.content.startsWith(prefix)) return;
+
+        let imagepf = message.author.displayAvatarURL();
+        let deletembed = new Discord.MessageEmbed()
+            .setColor("#d442f5")
+            .setAuthor(message.author.tag, imagepf)
+            .setDescription(`**Mensagem de ${message.author} apagada em ${message.channel}**`)
+            .addField("__Conteudo__", message.content)
+            .setFooter(`${message.author.username} ID: ${message.author.id} | ID da Mensagem: ${message.id}`)
+            .setTimestamp();
+
+        bot.channels.cache.get('454111723872321536').send(deletembed)
+    }
+}
