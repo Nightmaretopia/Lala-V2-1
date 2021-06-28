acf92307eadd2c8d902d0b0970cc39fd19b4a4ac
ChasJC23 <chas.coxell@gmail.com>
2021-06-28 22:53:35 +0100

    added fromHex function
    Included a fromHex function which generates a color from a hex value of varying length. Changes have been made to the rainbow function for robustness when combined with other functions and a unit test is now present at the end of the file.

--- colors.js
+++ colors.js
@@ -84,15 +84,94 @@ function fromHSV(hue, saturation, value) {
     }
 }
 
+function fromHex(hex) {
+
+    // remove any leading formatting characters
+    hex = hex.replace("#", "");
+
+    const colorValue = Number.parseInt(hex, 16);
+
+    let r;
+    let g;
+    let b;
+
+    switch (hex.length) {
+
+        // 8-bit color
+        case 2:
+            r = (colorValue & 0b1110_0000) / 0b1110_0000;
+            g = (colorValue & 0b0001_1100) / 0b0001_1100;
+            b = (colorValue & 0b0000_0011) / 0b0000_0011;
+            break;
+
+        // 12-bit color
+        case 3:
+            r = (colorValue & 0xF00) / 0xF00;
+            g = (colorValue & 0x0F0) / 0x0F0;
+            b = (colorValue & 0x00F) / 0x00F;
+            break;
+
+        // 16-bit color
+        case 4:
+            r = (colorValue & 0xF800) / 0xF800;
+            g = (colorValue & 0x07E0) / 0x07E0;
+            b = (colorValue & 0x001F) / 0x001F;
+            break;
+
+        // 24-bit color
+        case 6:
+            r = (colorValue & 0xFF0000) / 0xFF0000;
+            g = (colorValue & 0x00FF00) / 0x00FF00;
+            b = (colorValue & 0x0000FF) / 0x0000FF;
+            break;
+        
+        // 36-bit color
+        case 9:
+            r = (colorValue & 0xFFF000000) / 0xFFF000000;
+            g = (colorValue & 0x000FFF000) / 0x000FFF000;
+            b = (colorValue & 0x000000FFF) / 0x000000FFF;
+            break;
+        
+        // 48-bit color
+        case 12:
+            r = (colorValue & 0xFFFF_0000_0000) / 0xFFFF_0000_0000;
+            g = (colorValue & 0x0000_FFFF_0000) / 0x0000_FFFF_0000;
+            b = (colorValue & 0x0000_0000_FFFF) / 0x0000_0000_FFFF;
+            break;
+
+        default:
+            throw new Error("Invalid color format");
+    }
+
+    return new Color(r, g, b);
+}
+
 // color a given string of text in a rainbow style
 function rainbow(logMsg, isBg = false, speed = 0.1) {
+
     let result = ""
     logMsg = logMsg.replace(resetToken, "");
-    for (let h = 0; h < logMsg.length; h++) {
+    let h = 0;
+
+    for (let i = 0; i < logMsg.length; i++) {
+
+        if (logMsg[i] == '\u001B') {
+            do {
+                result += logMsg[i];
+                i++;
+            } while (logMsg[i] != 'm');
+            result += logMsg[i];
+            i++;
+        }
+
         const color = fromHSV((h * speed) % 1, 1, 1);
+
         result += isBg ? colorBGToken(color) : colorFGToken(color);
-        result += logMsg[h];
+        result += logMsg[i];
+
+        h++;
     }
+
     result += resetToken;
     return result;
 }
@@ -104,6 +183,7 @@ module.exports = {
     colorBG,
     colorFG,
     fromHSV,
+    fromHex,
     rainbow,
     BLACK: new Color(0, 0, 0),
     RED: new Color(1, 0, 0),
@@ -113,4 +193,28 @@ module.exports = {
     MAGENTA: new Color(1, 0, 1),
     YELLOW: new Color(1, 1, 0),
     WHITE: new Color(1, 1, 1)
+}
+
+if (process.argv[1] == __filename) {
+
+    let myColor = new Color(0.9, 0.2, 0.2);
+
+    let msg = colorBG("Hello World!", myColor);
+    console.log(msg);
+
+    myColor = fromHex("#198ed7");
+    msg = colorFG(msg, myColor);
+    console.log(msg);
+
+    myColor = fromHSV(0.48, 0.9, 0.3);
+    msg = colorBG("HSV teal!", myColor);
+    console.log(msg);
+
+    console.log(rainbow("Here's a rainbow!"));
+
+    msg = rainbow("I love rainbows, don't you? They're pretty awesome", true, 0.05);
+    console.log(colorFG(msg, module.exports.BLACK));
+
+    msg = colorFG("I love rainbows, don't you? They're pretty awesome", module.exports.BLACK);
+    console.log(rainbow(msg, true, 0.02));
 }
