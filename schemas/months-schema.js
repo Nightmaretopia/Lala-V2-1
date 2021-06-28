61fecd864c598c2675f5818e9bcd76af43ea7812
Didas <didasoficial@gmail.com>
2021-06-28 16:36:32 +0100

    Added months schema to log the new and old months
    This will log the month (jan, feb, etc...) how many coin usages there was and the percentage of that month

---
+++ schemas/months-schema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const reqString = {
+    type: String,
+    require: true,
+};
+
+const numb = {
+    type: Number,
+    default: 0
+};
+
+const monthSchema = mongoose.Schema({
+    __id: numb,
+    month: reqString + numb,
+    usages: numb,
+    percentage: numb
+});
+
+module.exports = mongoose.model('Months', monthSchema);
--- schemas/profile-schema.js
+++ schemas/profile-schema.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const reqString = {
     type: String,
     require: true,
-}
+};
 
 const numb = {
     type: Number,
     default: 0
-}
+};
 
 const profileSchema = mongoose.Schema({
     _id: reqString,
@@ -18,6 +18,6 @@ const profileSchema = mongoose.Schema({
     level: numb,
     reputation: numb,
     gifts: numb
-})
+});
 
-module.exports = mongoose.model('XP', profileSchema)
+module.exports = mongoose.model('XP', profileSchema);
