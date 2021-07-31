e35757884de573ac12dff203e2ff439e9894e705
DidaS <didasoficial@gmail.com>
2021-07-31 18:23:10 +0100

    Little update

--- events/annoy_toushiro.js
+++ events/annoy_toushiro.js
@@ -6,10 +6,7 @@ module.exports = {
     async execute(message) {
         let toushiro = '826306764320407554';
         if (message.author.bot) return;
-        if (message.author.id !== toushiro) {
-            //return logs.red('rip');
-        } else {
+        if (message.author.id !== toushiro) return;
             message.react('854359427570466817')
-        }
     }
 }
