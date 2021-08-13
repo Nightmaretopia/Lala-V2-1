e254a1193902c957b661b64d984fcbb99eb5010e
Didas <didasoficial@gmail.com>
2021-08-13 19:03:36 +0100

    Changed mongo login

--- mongo.js
+++ mongo.js
@@ -4,6 +4,7 @@ const { mongoPath } = require('./config.json')
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useNewUrlParser: true,
+        useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
     })
