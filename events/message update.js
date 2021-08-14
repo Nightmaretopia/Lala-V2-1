1428fd1dace75ace9cf7d1c4ee032d30c34e9f78
Didas <didasoficial@gmail.com>
2021-08-14 01:25:26 +0100

    Fixed my brain

--- events/message update.js
+++ events/message update.js
@@ -7,7 +7,7 @@ module.exports = {
         if (oldMsg.partial) return;
         if (!oldMsg.guild) return;
         if (!oldMsg.content) return;
-        if (oldMsg.username.bot) return;
+        if (oldMsg.user.bot) return;
         if (newMsg.content === oldMsg.content) return;
     
         let pfimage = newMsg.author.displayAvatarURL()
